cf9a00f56a39f16b98cbb85b9d2d55ed4b2d66a4
Eliran Hasin <eliranh1@cs.colman.ac.il>
2019-11-30 21:30:09 +0200

    WIP

--- src/components/side-navigation/SideBar.js
+++ src/components/side-navigation/SideBar.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Layout, Menu, Icon } from 'antd';
+import { withRouter } from "react-router";
+
 import { SideBarStyle } from './SideBarStyle';
 
 const { Sider } = Layout;
@@ -17,35 +19,56 @@ const renderSubMenuTitle = (iconType, text) => {
 const SideBar = props => {
     const [collapsed, setCollapsed] = useState(false);
 
+    const handleRoutesChanged = path => props.history.push(path);
 
     return (
-        <SideBarStyle>
-            <Layout id={'sider-layout'} style={{ minHeight: '100vh' }}>
-                <Sider width={200} collapsible collapsed={collapsed} onCollapse={col => setCollapsed(col)} className='sider-container'>
-                    <Menu
-                        mode="inline"
-                        theme="dark"
-                        defaultSelectedKeys={['profile']}
-                        defaultOpenKeys={['sub1']}
-                        className='sider-menu-container'>
-                        <Menu.Item key={'profile'}>{renderSubMenuTitle('profile', 'Profile')}</Menu.Item>
-                        <SubMenu key={'control'} title={renderSubMenuTitle('control', 'Controller')}>
-                            <Menu.Item key='clients'>Clients</Menu.Item>
-                            <Menu.Item key='posts'>Posts</Menu.Item>
-                        </SubMenu>
-                        <SubMenu key="calender" title={renderSubMenuTitle('calendar', 'Calendar')}>
-                            <Menu.Item key="cal">My Calendar</Menu.Item>
-                            <Menu.Item key="actions">Actions</Menu.Item>
-                            <Menu.Item key="waiting">Waiting List</Menu.Item>
-                        </SubMenu>
-                        <Menu.Item key='reports'>{renderSubMenuTitle('dashboard', 'Reports')}</Menu.Item>
 
-                    </Menu>
-                </Sider>
-            </Layout>
-        </SideBarStyle>
+        <Sider width={200} collapsible collapsed={collapsed} onCollapse={col => setCollapsed(col)} className='sider-container'>
+            {
+                !collapsed && <Menu
+                    mode="inline"
+                    theme="dark"
+                    defaultSelectedKeys={['profile']}
+                    defaultOpenKeys={['sub1']}
+                    className='sider-menu-container'>
+                    <Menu.Item key={'profile'} onClick={() => handleRoutesChanged('/profile')}>{renderSubMenuTitle('profile', 'Profile')}</Menu.Item>
+                    <SubMenu key={'control'} title={renderSubMenuTitle('control', 'Controller')}>
+                        <Menu.Item key='users' onClick={() => handleRoutesChanged('/controllers/users')}>Clients</Menu.Item>
+                        <Menu.Item key='posts' onClick={() => handleRoutesChanged('/controllers/posts')}>Posts</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="calender" title={renderSubMenuTitle('calendar', 'Calendar')}>
+                        <Menu.Item key="cal" onClick={() => handleRoutesChanged('/calendar')}>My Calendar</Menu.Item>
+                        <Menu.Item key="actions">Actions</Menu.Item>
+                        <Menu.Item key="waiting">Waiting List</Menu.Item>
+                    </SubMenu>
+                    <Menu.Item key='reports' onClick={() => handleRoutesChanged('/reports')}>{renderSubMenuTitle('pie-chart', 'Reports')}</Menu.Item>
+
+                </Menu>
+            }
+            {
+                collapsed && <Menu
+                    mode="inline"
+                    theme="dark"
+                    defaultSelectedKeys={['profile']}
+                    defaultOpenKeys={['sub1']}
+                    className='sider-menu-container'>
+                    <Menu.Item key={'profile'}>{renderSubMenuTitle('profile', '')}</Menu.Item>
+                    <SubMenu key={'control'} title={renderSubMenuTitle('control', '')}>
+                        <Menu.Item key='clients'>Clients</Menu.Item>
+                        <Menu.Item key='posts'>Posts</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="calender" title={renderSubMenuTitle('calendar', '')}>
+                        <Menu.Item key="cal">My Calendar</Menu.Item>
+                        <Menu.Item key="actions">Actions</Menu.Item>
+                        <Menu.Item key="waiting">Waiting List</Menu.Item>
+                    </SubMenu>
+                    <Menu.Item key='reports'>{renderSubMenuTitle('pie-chart', '')}</Menu.Item>
+
+                </Menu>
+            }
+        </Sider>
     )
 
 }
 
-export default SideBar;
+export default withRouter(SideBar);
